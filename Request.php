ccedaa8d3bbff01c30a293f112b2277f3314f5c3
SvenAlHamad <sven@webiny.com>
2015-02-05 03:37:43 +0000

    Version bumped to 1.2.1

--- Request.php
+++ Request.php
@@ -301,6 +301,17 @@ class Request
     }
 
     /**
+     * This method sets the internal value of currentUrl to $url.
+     * This method will not actually do a redirect, it is used mostly for mocking the internal value.
+     *
+     * @param string $url Current url.
+     */
+    public function setCurrentUrl($url)
+    {
+        $this->_currentUrl = $url;
+    }
+
+    /**
      * Get client ip address.
      * This function check and validates headers from trusted proxies.
      *
@@ -381,9 +392,14 @@ class Request
      */
     public function getConnectionPort()
     {
-        $host = $this->str($this->server()->httpHost());
-
         $port = 80;
+        $host = $this->server()->httpHost();
+        if(empty($host)){
+            return $port;
+        }
+
+        $host = $this->str($host);
+
         if($host->contains(':')){
             $port = $host->explode(':')->last();
         }
